24cc744cd17ed36f43f03567af8a0491e0648b31
mariagarm <maria.gmunoz@alumnos.upm.es>
2023-04-18 10:56:25 -0500

    Update h20mermaids.c

--- h20mermaids.c
+++ h20mermaids.c
@@ -1,9 +1,60 @@
 #include<stdio.h>
+
 int main(){
-	printf("Hola mundo");
-	printf("Hola mundo");
-	printf("Adios mundo");
-	return 0;
+	int opcion1;
+	printf("Bienvenido al buscador ...\n");
+    printf("Elige entre una de estas opciones:\n");
+    printf("1. Registrarte\n");
+    printf("2. Iniciar sesion\n");
+    printf("3. Salir\n");
+    scanf("%d", &opcion1);
+    
+    switch (opcion1) {
+    	case 1:
+    		
+    		break;
+    	case 2:
+    		
+    		break;
+    		
+    	case 3:
+    		printf("Hasta pronto\n");
+    		break;
+    	default:
+    		printf("Opcion invalida\n");
+    		break;
+	}
+	
+	int opcion2;
+	printf("Haz iniciado sesion correctamente\n");
+    printf("Elige entre una de estas opciones:\n");
+    printf("1. Agregar datos\n");
+    printf("2. Consultar datos\n");
+    printf("3. Salir\n");
+    scanf("%d", &opcion2);
+    
+    switch (opcion2) {
+    	case 1:
+    		
+    		break;
+    	case 2:
+    		
+    		break;
+    		
+    	case 3:
+    		printf("Hasta pronto\n");
+    		break;
+    	default:
+    		printf("Opcion invalida\n");
+    		break;
+	}
+	
+return 0;
 }
+	
+	
+	
+
+
 
 
