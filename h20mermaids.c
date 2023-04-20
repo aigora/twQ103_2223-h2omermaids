75184000b80b5c0e963bec1679277bcc04af595b
mariagarm <maria.gmunoz@alumnos.upm.es>
2023-04-20 10:10:33 -0500

    Update h20mermaids.c

--- h20mermaids.c
+++ h20mermaids.c
@@ -1,13 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-	void registrar();
-	 void inicio();
-	 
 int main(){
-	
 	int opcion1;
-	
 	printf("Bienvenido al buscador ...\n");
     printf("Elige entre una de estas opciones:\n");
     printf("1. Registrarte\n");
@@ -17,20 +11,19 @@ int main(){
     
     switch (opcion1) {
     	case 1:
-    		registrar()	;
+    		
     		break;
     	case 2:
-    		inicio();
+    		
     		break;
     		
     	case 3:
     		printf("Hasta pronto\n");
     		break;
     	default:
-    		printf("Opcion invalida. Intente de nuevo.\n");
+    		printf("Opcion invalida\n");
     		break;
-		}
-	
+	}
 	
 	int opcion2;
 	printf("Haz iniciado sesion correctamente\n");
@@ -55,64 +48,9 @@ int main(){
     		printf("Opcion invalida\n");
     		break;
 	}
-	return 0;
-	}
-	void registrar(){
-	typedef struct {
-		char nombre[50];
-		char apellido[50];
-		char DNI[9];
-	} Registro;
 	
-		Registro m ;
-   		FILE *usuarios;
-
-   printf("Ingrese su nombre:\n");
-   scanf("%s", m.nombre);
-
-   printf("Ingrese su apellido:\n ");
-   scanf("%s", m.apellido);
-
-   printf("Ingrese su DNI:\n");
-   scanf("%s", m.DNI);
-
-   usuarios = fopen("usuarios.txt", "a");
-   
-   fprintf(usuarios, "%s %s %s\n", m.nombre, m.apellido, m.DNI);
-	fclose(usuarios);
-
-   printf("\nSus datos han sido guardados\n");	
-	}
-	
-
-	void inicio(){
-		typedef struct {
-		char nombre[50];
-		char apellido[50];
-		char DNI[9];
-	} Ingreso;
-	
-	Ingreso a;
-	 printf("Ingrese su nombre:\n");
-   scanf("%s", a.nombre);
-
-   printf("Ingrese su apellido:\n ");
-   scanf("%s", a.apellido);
-
-   printf("Ingrese su DNI:\n");
-   scanf("%s", a.DNI);
-
-	FILE* ususarios =fopen("usuarios.txt", "r")	;
-	if (usuarios == NULL){
-		printf("Error al abrir el fichero\n");
-		return;
-	}
-	
-	
-	
-	
-	
-	}
+return 0;
+}
 	
 	
 	
